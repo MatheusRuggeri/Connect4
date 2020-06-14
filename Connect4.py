621116b3557091031f4bc1e90a188c75b0f35245
jomarumu <jomarumu@yahoo.com.br>
2020-06-13 22:27:15 -0300

    Update Connect4.py
    Cleaning the code

--- Connect4.py
+++ Connect4.py
@@ -2,65 +2,45 @@
 """
 Created on Sat May  9 20:42:18 2020
 
-@author: jomar
+@author: Matheus Ruggeri
 """
 
-import array
-
-nlin = 5
-ncol = 7
+# Número de linhas, colunas e quantas peças devem ser ligadas Connect4 -> k = 4
+nlin = 4
+ncol = 4
 k = 4
 
-IMPOSSIVEL = 0
-POSSIVEL   = 1
-
-BRANCO  = ' '
+# Estados possíveis para o tabuleiro
+VAZIO   = ' '
 MOLDURA = 'm'
-NULL    = 'n'
 XIS     = 'X'
 BOLA    = 'O'
-EMPATE  = 'E'
-
-HUMANO     = 'h'
-COMPUTADOR = 'c'
     
 def main():
-    tabuleiro = [["m"]*11 for i in range(11)]
-    fim = False
-    cont = 0
+    tabuleiro = [["m"]*(ncol+2) for i in range(nlin+2)]
     vez = XIS
-    
-    coloca_moldura(tabuleiro)
-    while (fim != True):
+    inicializa_tabuleiro(tabuleiro)
+    while (True):
         colNum = 0
-        while (joga(tabuleiro, colNum, vez) == IMPOSSIVEL):
+        while (joga(tabuleiro, colNum, vez) == False):
             colNum = input("Dê o número da coluna ou (q)uit:\t")
-            if (colNum.upper() == 'Q'):
+            if (colNum.upper() == 'Q' or colNum.upper() == 'QUIT'):
                 return 0
-            elif not(colNum.isdigit()):
-                colNum = 0
-            elif (int(colNum) < 1 or int(colNum) > ncol):
+            elif (not(colNum.isdigit()) or (int(colNum) < 1 or int(colNum) > ncol)):
                 colNum = 0
             else:
                 colNum = int(colNum)
         imprime_tabuleiro(tabuleiro)
-        cont = conta_ligados(tabuleiro,colNum)
-        print(cont)
-        if (cont >= k):
-            fim = True
-            vencedor = vez
+        if (conta_ligados(tabuleiro,colNum) >= k):
+            print("O jogador "+ vez + " ganhou!")
+            return 0
         elif (verifica_tabuleiro_cheio(tabuleiro) == True):
-            fim = True
-            vencedor = EMPATE
-        if(vez == XIS):
-            vez = BOLA
-        elif(vez == BOLA):
-            vez = XIS
+            print("O jogo terminou empatado")
+            return 0
+        vez = BOLA if (vez == XIS) else XIS
         
-    print("O jogador "+ vencedor + " ganhou!")
-
 # INICIALIZA O TABULEIRO
-def coloca_moldura(tabuleiro):
+def inicializa_tabuleiro(tabuleiro):
     for i in range(0, nlin+2):
         tabuleiro[i][0] = MOLDURA
         tabuleiro[i][ncol+1] = MOLDURA
@@ -69,112 +49,79 @@ def coloca_moldura(tabuleiro):
         tabuleiro[nlin+1][j] = MOLDURA
     for i in range(1,nlin+1):
         for j in range(1,ncol+1):
-            tabuleiro[i][j] = BRANCO
+            tabuleiro[i][j] = VAZIO
     
 # IMPRIME O TABULEIRO
 def imprime_tabuleiro(tabuleiro): 
     i = 1
     j = 0
-    contalin = 1
-    contacol = 1
-
+    lin = 1
+    col = 1
     while (i<nlin+1):
         print("   ", end = '')
         for j in range(1,ncol+1):
              print("+---", end = '')
-        print("+\n", end = '')
-        print(" ", end = '')
-        print(contalin, end = '')
-        contalin += 1
-        print(" ", end = '')
+        print("+")
+        print(" " + str(lin), end = ' ')
+        lin += 1
         for j in range(1,ncol+1):
-            print("| ", end = '')
-            if(tabuleiro[i][j] == BOLA):
-                print("O ", end = '')
-            elif(tabuleiro[i][j] == XIS):
-                print("X ", end = '')
-            elif(tabuleiro[i][j] == MOLDURA):
-                print("M ", end = '')
-            elif(tabuleiro[i][j] == BRANCO):
-                print(". ", end = '')
-            else:
-                print("  ", end = '')
+            print("| " + tabuleiro[i][j], end = ' ')
         i += 1
         print("|\n", end = '')
     if (i == nlin+1):
-        print("   ", end = '')
+        print("   ", end = '+')
         for j in range(1,ncol+1):
-            print("+---", end = '')
-        print("+\n", end = '')
-        print("   ", end = '')
+            print("---+", end = '')
+        print("\n     ", end = '')
         for j in range(1,ncol+1):
-            print("  "+str(contacol)+" ", end = '')
-            contacol += 1
+            print(str(col), end = '   ')
+            col += 1
     print("")
 
 def verifica_tabuleiro_cheio(tabuleiro):
     j = 1
     cheio = True
+    # Só precisa checar a primeira linha devido a gravidade presente no jogo
     while (j <= ncol and cheio == True):
-        if (tabuleiro[1][j] == BRANCO):
+        if (tabuleiro[1][j] == VAZIO):
             cheio = False
         j += 1
     return cheio
 
-def determina_vez(tabuleiro):
-    num_x = 0; num_b = 0;
-    i = 1; j = 1;
-    retorno = XIS
-    
-    for i in range(1,nlin+1):
-        for j in range(1,ncol+1):
-            if (tabuleiro[i][j] == BOLA):
-                num_b += 1
-            if (tabuleiro[i][j] == XIS):
-                num_x += 1
-    if (num_x > num_b):
-        retorno = BOLA
-    return retorno
-
 def joga(tabuleiro, col, tipo):
-    possibilidade = IMPOSSIVEL
-    salvo = False
-    lin = 9 #Usa o valor máximo
-    
-    if (tabuleiro[1][col] == BRANCO):
-        possibilidade = POSSIVEL
-        while (salvo == False and lin > 0):
-            if (tabuleiro[lin][col] == BRANCO):
+    # Usa o valor máximo da linha e vai decrescendo, retorna se foi possível fazer a jogada
+    lin = nlin
+    ehpossivel = False
+    jogou = False
+    if (tabuleiro[1][col] == VAZIO):
+        ehpossivel = True
+        while (jogou == False and lin > 0):
+            if (tabuleiro[lin][col] == VAZIO):
                 tabuleiro[lin][col] = tipo
-                salvo = 1
+                jogou = True
             else:
                 lin -= 1
-    else:
-        possibilidade = IMPOSSIVEL
-    return possibilidade
+    return ehpossivel
 
 def conta_ligados(tabuleiro, coluna):
-    nlin = 1
-    
-    while (tabuleiro[nlin][coluna] == MOLDURA or tabuleiro[nlin][coluna] == BRANCO):
-        nlin += 1
-    tipo = tabuleiro[nlin][coluna]
-    print (tipo)
-    
+    lin = 1
     direct = 1
     i = 1; j = 1;
     lig = 1
     maxLig = 1
     left = True
     right = True
+    while (tabuleiro[lin][coluna] == MOLDURA or tabuleiro[lin][coluna] == VAZIO):
+        lin += 1
+    tipo = tabuleiro[lin][coluna]
     while (direct <= 4):
         if (right):
-            if (tabuleiro[nlin+i][coluna+j] == tipo):
+            if (tabuleiro[lin+i][coluna+j] == tipo):
                 lig += 1
             else:
                 right = False
         if (left):
-            if (tabuleiro[nlin-i][coluna-j] == tipo):
+            if (tabuleiro[lin-i][coluna-j] == tipo):
                 lig += 1
             else:
                 left = False
@@ -186,19 +133,17 @@ def conta_ligados(tabuleiro, coluna):
             j = 0
             if (lig > maxLig):
                 maxLig = lig
-            lig = 1
-            
-        if (direct == 1): # Diag \
+            lig = 1            
+        if (direct == 1): # Diag -> \ <-
             i += 1
             j += 1
-        if (direct == 2): # Diag /
+        if (direct == 2): # Diag -> / <-
             i -= 1
             j += 1
-        if (direct == 3): # Diag |
+        if (direct == 3): # Col  -> | <-
             i += 1
-        if (direct == 4): # Diag -
+        if (direct == 4): # Lin  -> - <-
             j += 1
-        
     return maxLig
 
 main()
